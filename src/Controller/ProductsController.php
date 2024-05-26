f9551fc2cb0d3601db804bc298e04805566e0257
kristapsvitols <kristapsvitols91@gmail.com>
2024-05-26 10:13:56 +0300

    wip

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -1,8 +1,40 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
-class ProductsController
-{
+use App\DTO\LowestPriceEnquiry;
+use App\Filter\PromotionsFilterInterface;
+use App\Service\Serializer\DTOSerializer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class ProductsController extends AbstractController
+{
+    #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
+    public function lowestPrice(
+        Request $request,
+        int $id,
+        DTOSerializer $serializer,
+        PromotionsFilterInterface $promotionsFilter
+    ): Response {
+        if ($request->headers->has('force_fail')) {
+            return new JsonResponse([
+                'error' => 'Promotions Engine failure message',
+            ], (int)$request->headers->get('force_fail'));
+        }
+
+        /** @var LowestPriceEnquiry $lowestPriceEnquiry */
+        $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
+
+        $modifiedEnquiry = $promotionsFilter->apply($lowestPriceEnquiry);
+
+        $responseContent = $serializer->serialize($modifiedEnquiry, 'json');
+
+        return new Response($responseContent, 200, ['Content-Type' => 'application/json']);
+    }
 }
